c12c990980cd7e54576e96a7ddb8fdb2ba68cbaa
Ítalo Vinícius <italovinicius18@yahoo.com>
2023-05-05 19:22:55 -0300

    Initial environment

---
+++ src/configs/configuration.ts
@@ -0,0 +1,11 @@
+export default () => ({
+  database: {
+    host: 'postgres',
+    user: 'postgres',
+    pass: 'postgres',
+    db: 'schedula_tutorial',
+    address: 'schedula_tutorial_db',
+    port: 5105,
+  },
+  environment: 'DEVELOPMENT',
+});
